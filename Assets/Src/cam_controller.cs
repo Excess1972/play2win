d8bf2ea886f924095a44cbe2925ca1264d69d706
Excess1972 <excess1972@gmail.com>
2022-12-03 00:54:04 +0100

    refactor cam movement to mobile compatible style

--- Assets/Src/cam_controller.cs
+++ Assets/Src/cam_controller.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 public class cam_controller : MonoBehaviour
 {
-	private Camera _cam;
-	private float  _speed = 20;
+	private Camera  _cam;
+	private float   _speed = 20;
+	private Vector3 touchStart;
 
 	private void Awake()
 	{
@@ -13,41 +14,58 @@ public class cam_controller : MonoBehaviour
 
 	void Update()
 	{
-		float wheelVal = -Input.GetAxis("Mouse ScrollWheel");
-		if (wheelVal != 0f)
+		// float wheelVal = -Input.GetAxis("Mouse ScrollWheel");
+		// if (wheelVal != 0f)
+		// {
+		// 	float fov = Mathf.MoveTowards(
+		// 		_cam.fieldOfView,
+		// 		60,
+		// 		wheelVal * 2000 * Time.deltaTime
+		// 	);
+		// 	_cam.fieldOfView = Mathf.Clamp(fov, 20, 40);
+		// 	return;
+		// }
+
+		if (Input.GetMouseButtonDown(0))
 		{
-			float fov = Mathf.MoveTowards(
-				_cam.fieldOfView,
-				60,
-				wheelVal * 2000 * Time.deltaTime
-			);
- 			_cam.fieldOfView = Mathf.Clamp(fov, 20, 40);
-			return;
+			touchStart = GetWorldPosition(0);
 		}
-
-		var pos = transform.position;
-
-		// up
-		if (Input.GetKey("w"))
-		{
-			pos.z += _speed * Time.deltaTime;
-		}
-
-		// down
-		if (Input.GetKey("s"))
-			pos.z -= _speed * Time.deltaTime;
-
-		// left
-		if (Input.GetKey("a"))
-			pos.x -= _speed * Time.deltaTime;
-
-		// right
-		if (Input.GetKey("d"))
-			pos.x += _speed * Time.deltaTime;
 		
-		pos.x = Mathf.Clamp(pos.x, -40, 40);
-		pos.z = Mathf.Clamp(pos.z, -60, 10);
-
-		transform.position = pos;
+		if (Input.GetMouseButton(0)){
+			Vector3 direction = touchStart - GetWorldPosition(0);
+			_cam.transform.position += direction;
+		}
+		// var pos = transform.position;
+		//
+		// // up
+		// if (Input.GetKey("w"))
+		// {
+		// 	pos.z += _speed * Time.deltaTime;
+		// }
+		//
+		// // down
+		// if (Input.GetKey("s"))
+		// 	pos.z -= _speed * Time.deltaTime;
+		//
+		// // left
+		// if (Input.GetKey("a"))
+		// 	pos.x -= _speed * Time.deltaTime;
+		//
+		// // right
+		// if (Input.GetKey("d"))
+		// 	pos.x += _speed * Time.deltaTime;
+		//
+		// pos.x = Mathf.Clamp(pos.x, -40, 40);
+		// pos.z = Mathf.Clamp(pos.z, -60, 10);
+		//
+		// transform.position = pos;
+	}
+	
+	private Vector3 GetWorldPosition(float z){
+		Ray   mousePos = _cam.ScreenPointToRay(Input.mousePosition);
+		Plane ground   = new Plane(Vector3.down, new Vector3(0,0,z));
+		float distance;
+		ground.Raycast(mousePos, out distance);
+		return mousePos.GetPoint(distance);
 	}
 }
